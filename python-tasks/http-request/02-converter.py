9a17879bbb3437d3ab43d514f585dc2d4567837e
Nanda Purba <oliviergiroud1998@gmail.com>
2020-09-04 16:58:43 +0700

    No 2 done, naive way

---
+++ python-tasks/http-request/02-converter.py
@@ -0,0 +1,58 @@
+istilah = {
+    1: "satu",
+    2: "dua",
+    3: "tiga",
+    4: "empat",
+    5: "liam",
+    6: "enam",
+    7: "tujuh",
+    8: "delapan",
+    9: "sembilan",
+    100: "seratus",
+    10: "sepuluh",
+    11: "sebelas"
+}
+
+def converter(e):
+    if 0 <= e <= 9:
+        return istilah[e]
+    if e == 10:
+        return "sepuluh"
+    if e == 11:
+        return "sebelas"
+    if e == 100:
+        return "seratus"
+    if 12 <= e <= 19:
+        _, remainder = divmod(e, 10)
+        return "{} belas".format(istilah[remainder])
+    if 20 <= e <= 99:
+        val, remainder = divmod(e, 10)
+        return "{} puluh {}".format(istilah[val], istilah[remainder])
+    if 101 <= e <= 199:
+        val, remainder = divmod(e, 100)
+        if 1 <= remainder <= 19:
+            return "{} {}".format(istilah[val * 100], istilah[remainder])
+        else:
+            x, y = divmod(remainder, 10)
+            if y == 0:
+                return "{} {} puluh".format(istilah[val * 100], istilah[x])
+            return "{} {} puluh {}".format(istilah[val * 100], istilah[x], istilah[y])
+    if 200 <= e <= 999:
+        val, remainder = divmod(e, 100)
+        if remainder == 0:
+            return "{} ratus".format(istilah[val])
+        if 1 <= remainder <= 19:
+            return "{} ratus {}".format(istilah[val], istilah[remainder])
+        else:
+            x, y = divmod(remainder, 10)
+            if y == 0:
+                return "{} ratus {} puluh".format(istilah[val], istilah[x])
+            return "{} ratus {} puluh {}".format(istilah[val], istilah[x], istilah[y])
+            
+
+if __name__ == "__main__":
+    print(converter(2))
+    print(converter(18))
+    print(converter(34))
+    print(converter(189))
+    print(converter(201))
